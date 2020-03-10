bc50c63a89a22015f6a04d1fb79f969d505a953f
Daniel <artexsdns@gmail.com>
2020-03-10 14:13:13 +0700

    integration application accounting cloud

---
+++ config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  siteTitle: 'Integration application cloud', // Site title.
+  siteTitleAlt: 'Business', // Alternative site title for SEO.
+  siteLogo: '/icons/icons.png', // Logo used for SEO and manifest.
+  siteUrl: 'https://integration-application-cloud.netlify.com', // Domain of your website without pathPrefix.
+  pathPrefix: '', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
+  siteDescription: 'Platform application integration your business.', // Website description used for RSS feeds/meta description tag.
+  siteRss: '/rss.xml',
+  siteFBAppID: '', // FB Application ID for using app insights
+  googleTagManagerID: '', // GTM tracking ID.
+  disqusShortname: 'integration-application-cloud', // Disqus shortname.
+  userName: 'Daniel Budi Setyawan',
+  userTwitter: 'vaibhaved',
+  userLocation: 'Sedati, Sidarjo',
+  userDescription: '',
+  copyright: 'Copyright © Integration process business 2020. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
+  themeColor: '#00d1b2', // Used for setting manifest and progress theme colors.
+  backgroundColor: '#ffffff', // Used for setting manifest background color.
+}
