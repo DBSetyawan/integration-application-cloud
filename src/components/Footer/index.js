2ae51d59f5bf1f0ceb0ce136c98bb486ca7f6baf
daniel setyawan <artexsdns@gmail.com>
2020-03-13 15:21:19 +0700

    Upgrade share social media

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { graphql } from 'gatsby'
 import config from '../../../config'
+import Share from '../../components/Share'
 
-const Footer = () => {
+  const FooterPage = ({ data }) => {
+  // const { markdownRemark: post } = data
+
   return (
     <footer className='footer'>
       <div className='container'>
         <div className='content has-text-centered'>
+          {/* <Share
+            title={post.frontmatter.title}
+            slug={post.fields.slug}
+            excerpt={post.frontmatter.meta_description}
+          /> */}
           <p>Powered by <a href='https://www.gatsbyjs.org'>Gatsby</a> and <a href='https://www.netlifycms.org'>Netlify CMS</a> | Integration Application your business</p>
           <p>Developer by <a href='https://dbsetyawan.github.io/portfolio/'>Daniel Budi Setyawan</a> & All team.</p> 
           <p>
@@ -17,4 +27,23 @@ const Footer = () => {
   )
 }
 
-export default Footer
+FooterPage.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.object
+  }),
+}
+
+export default FooterPage
+
+export const pageQuery = graphql`
+  query IndexPageFooter($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      id
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        title
+      }
+    }
+  }
+`
--- src/templates/home-page.js
+++ src/templates/home-page.js
@@ -3,10 +3,11 @@ import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 import HomePageTemplate from '../components/HomePageTemplate'
 import Layout from '../components/Layout'
+import Share from '../components/Share'
 
 const HomePage = ({ data }) => {
   const { frontmatter } = data.markdownRemark
-
+  const { markdownRemark: post } = data
   return (
     <Layout>
       <HomePageTemplate
@@ -18,6 +19,11 @@ const HomePage = ({ data }) => {
         offerings={frontmatter.offerings}
         testimonials={frontmatter.testimonials}
       />
+        <Share
+          title={post.frontmatter.title}
+          slug={post.fields.slug}
+          excerpt={post.frontmatter.meta_description}
+        />
     </Layout>
   )
 }
@@ -35,6 +41,9 @@ export default HomePage
 export const pageQuery = graphql`
   query IndexPage($id: String!) {
     markdownRemark(id: { eq: $id }) {
+      fields {
+        slug
+      }
       frontmatter {
         title
         meta_title
